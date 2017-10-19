ffb0ef3aa0f2d341268998a7e2f570b589737f33
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-19 09:10:21 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,12 +5,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {:name => "Benvolio"
-        :age => "17"
+        {:name => "Benvolio",
+        :age => "17",
         :attitude => "worried"}
         {
-          :name => "Mercutio"
-          :age => "18"
+          :name => "Mercutio",
+          :age => "18",
           :attitude => "hot-headed"
         }
       ]
@@ -21,13 +21,13 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
           {
-            :name => "Steven"
-            :age => "30"
+            :name => "Steven",
+            :age => "30",
             :attitude => "confused"
           }
           {
-            :name => "Nurse"
-            :age => "44"
+            :name => "Nurse",
+            :age => "44",
             :attitude => " worried"
           }
 

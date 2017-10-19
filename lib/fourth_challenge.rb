b3af0306610cfa49bde29023b00e8d1b57eda8fa
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-19 09:11:21 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,7 +8,7 @@ def fourth_challenge
         {:name => "Benvolio",
         :age => "17",
         :attitude => "worried"},
-        
+
         {
           :name => "Mercutio",
           :age => "18",

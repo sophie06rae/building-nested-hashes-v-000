9a17b7a55dbb6a3d5e4295e1d8fa8167b76e265a
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-19 08:58:10 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,16 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch =>{},
-      :matriarch =>{},
-      :hero =>{},
-      :hero_firends =>[]
-      },
-    :capulet => {
-      :patriarch =>{},
-      :matriarch =>{},
-      :heroine =>{},
-      :heroine_firends =>[]
-      }
-    }
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
 end
